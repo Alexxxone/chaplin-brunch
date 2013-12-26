150cfa9e35e80ade4e1e6cf597aa204bf51f04f0
Alex <bulletone@mail.ru>
2013-12-26 10:27:56 +0200

    asd

--- web.js
+++ web.js
@@ -3,7 +3,12 @@ console.log(process.env.PORT);
 var statik = require('statik');
 
 statik(process.env.PORT || 3005);
-console.log(process.env.PORT);
 
+//
+//var statik = require('../index');
+//var server = statik.createServer();
+//
+//server.listen(3000);
+//console.log("server online at http://localhost:3000/");
 //brunch watch --server
 //http://backbonetutorials.com/cross-domain-sessions/
